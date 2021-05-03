10c0ac58d1b0a0f91678ce5c6b8d44bdf2e4b5ea
Paweł Łukaszuk <pawel-github@lukaszuk.net>
2021-05-03 22:28:57 +0200

    Tasks from the fourth classes

--- Semestr2/04/02/app.js
+++ Semestr2/04/02/app.js
@@ -7,39 +7,43 @@ const { ObjectId } = require('mongodb');
 const uri = process.env.MONGODB_CONNECTION;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // łączymy się z bazą danych
 client.connect(async err => {
     // wybieramy odpowiednią bazę danych oraz kolekcję
-    const collection = client.db("todo").collection("tasks");
+    const collection = client.db("mydataase").collection("users");
 
-    // pobieramy wszystkie elementy kolekcji 'tasks'
+    // pobieramy wszystkie elementy kolekcji 'users'
     console.log(await collection.find().toArray());
 
-    // dodawanie do kolekcji nowego zadania
+    // dodajemy do kolekcji nowego użytkownika
     const insertResult = await collection.insertOne({
-        task: 'kupic lizaki',
-        completed: false,
+        firstName: 'Jan',
+        lastName: 'Kowalsi',
+        isActive: true
     });
     console.log(insertResult);
 
+    // dodajemy do kolekcji nowego użytkownika z wybranym _id
+    const insertWithIdResult = await collection.insertOne({
+        firstName: 'Adam',
+        lastName: 'Nowak',
+        isActive: false,
+        _id: new ObjectId("507f191e810c19729de860eb")
+    });
+    console.log(insertWithIdResult);
 
-    console.log(await collection.find().toArray());
-
-
-    // aktualizacja dokumentów w kolekcji
-    const updateResult = await collection.updateMany({}, { $set: { createDate: '28.06.2020' } });
+    // zmieniamy imię wszystkich użytkowników
+    const updateResult = await collection.updateMany({}, { $set: { firstNAme: 'Marek' } });
     console.log(updateResult);
-    // await collection.updateOne({ _id: ObjectId('5ef8b39a4691151dc8df51ce') }, { $set : { completed: true }})
 
+    // pobieramy wszystkich aktywnych użytkowników
+    const findResult = await collection.find({ isActive: true }).toArray();
+    console.table(findResult);
 
-    console.log(await collection.find().toArray());
-
-    // kasowanie elementów 
-    const deleteResult = await collection.deleteMany({ _id: ObjectId('5ef8b39a4691151dc8df51ce') });
+    // usuwamy wszytkich nieaktywnych użytkowników
+    const deleteResult = await collection.deleteMany({ isActive: false });
     console.log(deleteResult);
-
-    console.log(await collection.find().toArray());
-
 
     client.close();
 });
